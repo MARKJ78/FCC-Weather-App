d82b7be77438d39c89891c5e911c8872022b4969
Mark Jones <mpjonesemail@gmail.com>
2018-07-16 16:23:21 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -336,11 +336,9 @@ function populate(response) {
     //Populate Current weather panel//////////////////////////////////////////////////////////////////////
     $('.weather .upper .bottom').html(response.current.condition.text);
     $('.weather .upper .main').html('<i class="wi ' + currentWeatherIcon + '"></i>');
-    $('.weather .sub1').html('UV' + '&nbsp;&nbsp;' + '<i class="wi ' + currentWeatherUv + '"></i>');
-    $('.weather .sub2').html('Humidity' + '&nbsp;&nbsp;' + '<i class="wi ' + currentWeatherHumidity + '"></i>');
-    $('.weather .sub3').html('Rain mm' + '&nbsp;&nbsp;' + '<i class="wi ' + currentWeatherPrecmm + '"></i>');
- 
-
+    $('.weather .sub1').html('UV' + '&nbsp;&nbsp;' + currentWeatherUv);
+    $('.weather .sub2').html('Humidity' + '&nbsp;&nbsp;' + currentWeatherHumidity);
+    $('.weather .sub3').html('Rain mm' + '&nbsp;&nbsp;' + currentWeatherPrecmm);
     //Populate current date panel ////////////////////////////////////////////////////////////////////////
     //Call current date from browser, format and populate
     var dateObj = new Date();
